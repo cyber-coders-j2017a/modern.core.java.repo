7a81b1014eda3e34cc857f89bfe600bdb447e23a
Tochukwu Anyanetoh <tccanini@yahoo.com>
2017-06-13 22:39:18 +0100

    project Navigator design specification

--- ForceIndia/src/ForceIndia1/Model2.java
+++ ForceIndia/src/ForceIndia1/Model2.java
@@ -7,7 +7,7 @@ public class Model2 {
     public static void main(String []args){
 
         for(double y=100;y>5;y--){
-            System.out.println("Welcome to world of Java"+ y);
+            System.out.println("Welcome to world of Java"+y);
         }
     }
 }
