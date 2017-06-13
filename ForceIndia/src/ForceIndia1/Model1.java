f600d419cac05581d8bc0864ad6f20049fcacf94
Tochukwu Anyanetoh <tccanini@yahoo.com>
2017-06-13 22:32:19 +0100

    java project using WHILE and FOR statements

---
+++ ForceIndia/src/ForceIndia1/Model1.java
@@ -0,0 +1,26 @@
+package ForceIndia1;
+
+/**
+ * Created by Kanini W'Tang on 6/13/2017.
+ */
+public class Model1 {
+    public static void main(String[]args){
+        int firstValue=3;
+        int x=1;
+        while (x<=50) {
+                System.out.println("Feedback control:" + firstValue * x);
+                x++;
+
+                   }
+
+        }
+
+        }
+
+
+
+
+
+
+
+
---
+++ ForceIndia/src/ForceIndia1/Model2.java
@@ -0,0 +1,13 @@
+package ForceIndia1;
+
+/**
+ * Created by Kanini W'Tang on 6/13/2017.
+ */
+public class Model2 {
+    public static void main(String []args){
+
+        for(double y=100;y>5;y--){
+            System.out.println("Welcome to world of Java"+ y);
+        }
+    }
+}
