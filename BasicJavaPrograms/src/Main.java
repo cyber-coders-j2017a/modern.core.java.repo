cd72acae9288368ac99cb67a1f5f0e5b56129bd0
Edwin Nwofor (Dotcom) <edwin.nwofor@yahoo.com>
2017-06-14 20:50:48 +0100

    Created a new Java project for the group!

---
+++ BasicJavaPrograms/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter your favourite number");
+
+        Scanner myInput = new Scanner(System.in);
+        String number = myInput.nextLine();
+
+        System.out.println(number);
+
+
+
+
+    }
+}
