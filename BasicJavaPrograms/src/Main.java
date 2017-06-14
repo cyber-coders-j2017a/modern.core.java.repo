29f56547c0ec8543d8a51e364f365b4636741fbc
Edwin Nwofor (Dotcom) <edwin.nwofor@yahoo.com>
2017-06-14 22:13:50 +0100

    Updated

--- BasicJavaPrograms/src/Main.java
+++ BasicJavaPrograms/src/Main.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Please enter your favourite number");
+
 
         Scanner myInput = new Scanner(System.in);
         String number = myInput.nextLine();
