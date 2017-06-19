24e0e8c1ede5e8bdce6d569dfa0636e123d8a1df
Edwin Nwofor (Dotcom) <edwin.nwofor@yahoo.com>
2017-06-19 21:29:29 +0100

    Implemneted Control statement

---
+++ JavaCorePrograms/src/samplesCodes/StudentsTest.java
@@ -0,0 +1,30 @@
+package samplesCodes;
+
+import java.util.Scanner;
+
+/**
+ * Created by dotcom on 6/19/17.
+ */
+public class StudentsTest {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String studentPosition = scan.nextLine();
+        int studPos = Integer.parseInt(studentPosition);
+
+
+
+
+        String [] studentNames = {"Jeff", "Jude", "Chika", "Tim", "Rita", "Naomi", "Ngozi", "Nick"};
+
+        for (int i = 1; i < 10; i++){
+            int a = i +1;
+            if(studPos == a){
+                String name = studentNames[i];
+                System.out.println("The student you are looking for is "+name);
+                break;
+            }
+        }
+    }
+}
