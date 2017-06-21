8f1ac136d2448e7ac2f2abcf456350f979341158
Edwin Nwofor (Dotcom) <edwin.nwofor@yahoo.com>
2017-06-21 22:14:53 +0100

    Implementation of Object Oriented Programming concept

---
+++ JavaCorePrograms/src/recap/session/OoProgConcept/Animal.java
@@ -0,0 +1,41 @@
+package recap.session.OoProgConcept;
+
+/**
+ * Created by dotcom on 6/21/17.
+ */
+public class Animal {
+
+    //Variables/Attributes
+    int numOflegs = 4;
+    String colour = "Black";
+    double height = 4.09D;
+    double weight = 56.99d;
+    float feed = 2.135f;
+    char bloodGroup = 'A';
+    boolean isWild = false;
+
+
+
+    //Methods
+    public void move(){
+        System.out.println("Yes I can move");
+        System.out.println("Yes I can move");
+        System.out.println("Yes I can move");
+    }
+
+
+    public void run(){
+        System.out.println("Yes I can run");
+        System.out.println("Yes I can run");
+        System.out.println("Yes I can run");
+    }
+
+    public void giveBirth(){
+        System.out.println("Yes an animal can give birth");
+    }
+
+    public void cry(){
+        System.out.println("Yes I can cry");
+    }
+
+}
