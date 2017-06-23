edd1e5c98a6e99e69b81fe132919e1e43e8edbe5
Jefferson <osharej@yahoo.com>
2017-06-23 15:36:04 +0100

    changes

---
+++ JavaPractice/src/Practice01/sample02.java
@@ -0,0 +1,13 @@
+package Practice01;
+
+/**
+ * Created by oddie on 23/06/2017.
+ */
+public class sample02 {
+public static void main(String []args){
+    String []weatherConditions ={"sunny","rainy", "windy"};
+    for(String weatherCondition : weatherConditions){
+        System.out.println("various weather conditions are "+ weatherCondition);
+    }
+  }
+}
