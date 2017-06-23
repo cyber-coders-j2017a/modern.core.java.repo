82dd91d312bc2ab244640eccb7c94a1ec5452ed2
Jefferson <osharej@yahoo.com>
2017-06-23 11:29:22 +0100

    new folder + oop codes

---
+++ Jeff_pet_codes/src/com/company/Phone.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Phone {
+
+    // variables
+
+    double weight = 600.5d;
+    double screanSize = 7.3d;
+    String colour = "gold jeff mixer";
+    String camera = "8 megaPixels";
+    String memory = "99 GB";
+    String battery = "up to 20hours talkTime";
+    String operationSystem = "Jff 1.2";
+    String screenType = "touch Screen";
+    String bodyMaterial = "jeff magnum";
+    boolean waterResistant = true;
+    boolean antiScratch = true;
+    boolean unbreakable = true;
+    boolean incombustable = true;
+    boolean unstealable = true;
+    boolean SimLess = true;
+
+
+
+    //methods
+
+    public void makeCalls()
+    {
+
+    }
+ }
