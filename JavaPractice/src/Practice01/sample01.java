4ebe27f801903a4db37cdec1d0e60284ba37ed0c
Jefferson <osharej@yahoo.com>
2017-06-26 20:33:15 +0100

    added code

--- JavaPractice/src/Practice01/sample01.java
+++ JavaPractice/src/Practice01/sample01.java
@@ -29,6 +29,7 @@ public class sample01 {
             System.out.println("Your child will go to reception class");
         }else if(age > 5 && age <= 11){
             System.out.println("Your child will be in key stage 2, which is from year one to year six");
-        }
+        } else if (age> 2 && age )
+            System.out.println("Your child will be in key stage 2, which is from year one to year six");
     }
 }
