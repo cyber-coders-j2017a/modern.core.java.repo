87eb07cbbb5632a62dfaa417333aefd528780a2d
Tochukwu Anyanetoh <tccanini@yahoo.com>
2017-06-29 19:11:36 +0100

    Codes of method practice created by Tochukwu

---
+++ Tim202_projects/Tim601Methods/src/Practice602/TimMethod2.java
@@ -0,0 +1,19 @@
+package Practice602;
+
+/**
+ * Created by Kanini W'Tang on 6/29/2017.
+ */
+public class TimMethod2 {
+
+    // The method practice - Modifier returnType nameOfMethod (Parameter list){//method body}
+
+    public static int maxFunction(int n1, int n2) {
+        int max;
+        if (n1 < n2)
+            max = n1;
+        else
+            max = n2;
+        return max;
+    }
+}
+
---
+++ Tim202_projects/Tim601Methods/src/Practice602/TimMethods1.java
@@ -0,0 +1,18 @@
+package Practice602;
+
+/**
+ * Created by Kanini W'Tang on 6/29/2017.
+ */
+public class TimMethods1 {
+    // The method practice - Modifier returnType nameOfMethod (Parameter list){//method body}
+
+    public static int minFunction(int m1, int m2){
+        int  min;
+        if (m1>m2)
+        min =m2;
+        else
+            min=m1;
+        return min;
+
+    }
+}
