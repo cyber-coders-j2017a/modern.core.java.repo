2e3684149df051dfd317d5e822ed04a041a7e9eb
Edwin Nwofor (Dotcom) <edwin.nwofor@yahoo.com>
2017-07-07 22:14:25 +0100

    Updated

--- JavaCorePrograms/src/fullcodes/session/accessModifierTwo/Cat.java
+++ JavaCorePrograms/src/fullcodes/session/accessModifierTwo/Cat.java
@@ -6,7 +6,7 @@ import fullcodes.session.accessModifiers.Barclays;
 /**
  * Created by dotcom on 6/30/17.
  */
-public  Cat extends Animal{
+public class Cat extends Animal{
 
 
     public void taste(){
