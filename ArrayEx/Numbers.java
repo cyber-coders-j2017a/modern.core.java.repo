0546eed560930ca90c3c5622031deca9a7bbfee5
Naomi Monplaisir <you@example.com>
2017-07-11 22:11:54 +0100

    New file added by Naomi

---
+++ ArrayEx/Numbers.java
@@ -0,0 +1,21 @@
+package ArrayEx;
+
+/**
+ * Created by LewisMonplaisir on 11/07/2017.
+ */
+public class Numbers {
+    public static void main(String[] args) {
+        int great[]=new int[5];
+
+        great[0]=43;
+        great[1]=36;
+        great[2]=65;
+        great[3]=77;
+        great[4]=100;
+
+        System.out.println(great[2]);
+
+    }
+
+
+}
