3cc76cc48f623ca8ceed1f147591dbaf18b465ae
Edwin Nwofor (Dotcom) <edwin.nwofor@yahoo.com>
2017-07-12 21:16:56 +0100

    Improved the code to include private method, searching for a student and tidying up codes

--- ClassCodes/src/fullcodes/session/twoDimArraysInJava/Student.java
+++ ClassCodes/src/fullcodes/session/twoDimArraysInJava/Student.java
@@ -5,44 +5,28 @@ package fullcodes.session.twoDimArraysInJava;
  */
 public class Student {
 
-    public void findStudent(){
-        //Declare an array
-        String[][] studentNames = new String[2][3]; //2 rows, 3 columns
-        //Initialize the array
-        studentNames[0][0] = "Joy";
-        studentNames[0][1] = "Happiness";
-        studentNames[0][2] = "Grace";
+    public void findStudent(String nameToSearch){
 
-        studentNames[1][0] = "Humphries";
-        studentNames[1][1] = "Johnston";
-        studentNames[1][2] = "Peters";
-
+        String[][]studentNames = this.getStudentNames();
         //Access arrays
 
         //Iterating over rows
         for(int r =0; r<studentNames.length; r++){
-
             //This is going to visit all the cells in a row(column by column
             for(int c=0; c<studentNames[r].length; c++){
-                System.out.println(studentNames[r][c]);
-            }
 
+                if(nameToSearch.equalsIgnoreCase(studentNames[r][c])){
+                    System.out.println("He is a registered student in this batch");
+                    return;
+                }
+            }
         }
+        System.out.println("There is no such a student in this class");
     }
 
     public void createStudentNames(){
 //        String carNames[][] = new String[3][2];
-        //Declare an array
-        String[][] studentNames = new String[2][3]; //2 rows, 3 columns
-        //Initialize the array
-        studentNames[0][0] = "Joy";
-        studentNames[0][1] = "Happiness";
-        studentNames[0][2] = "Grace";
-
-        studentNames[1][0] = "Humphries";
-        studentNames[1][1] = "Johnston";
-        studentNames[1][2] = "Peters";
-
+        String[][]studentNames = this.getStudentNames();
         //Access arrays
         System.out.println(studentNames[1][2]);
 
@@ -50,7 +34,7 @@ public class Student {
     }
 
 
-    public String[][] getStudentNames(){
+    private String[][] getStudentNames(){
         String[][] studentNames = new String[2][3]; //2 rows, 3 columns
         //Initialize the array
         studentNames[0][0] = "Joy";
--- ClassCodes/src/fullcodes/session/twoDimArraysInJava/TestStuRun.java
+++ ClassCodes/src/fullcodes/session/twoDimArraysInJava/TestStuRun.java
@@ -1,13 +1,18 @@
 package fullcodes.session.twoDimArraysInJava;
 
+import java.util.Scanner;
+
 /**
  * Created by dotcom on 7/12/17.
  */
 public class TestStuRun {
 
     public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("PLease enter the student to look for");
+        String name  = myScanner.nextLine();
         Student mike = new Student();
-        mike.findStudent();
+        mike.findStudent(name);
 
     }
 }
