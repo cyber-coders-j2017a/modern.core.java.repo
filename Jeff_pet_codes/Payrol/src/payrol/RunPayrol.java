20529975e85cf674dcee445650ade8f2024733ff
Jefferson <osharej@yahoo.com>
2017-07-14 18:06:06 +0100

    array

---
+++ Jeff_pet_codes/Payrol/src/payrol/RunPayrol.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payrol;
+
+/**
+ *
+ * @author Jeffries Pc
+ */
+public class RunPayrol {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        ThePayRoll payrol = new ThePayRoll();
+         payrol.displayEmployeeNames();
+         
+         payrol.showBonusPackages();
+       
+        // TODO code application logic here
+    }
+    
+}
