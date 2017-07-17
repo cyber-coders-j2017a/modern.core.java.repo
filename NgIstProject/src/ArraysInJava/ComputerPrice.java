8959967c63332bd2e0eaa7e3b3547807a7eb726e
beatrice-otugo <ngotugo@yahoo.co.uk>
2017-07-17 17:06:11 +0100

    added new code for arrays

---
+++ NgIstProject/src/ArraysInJava/ComputerPrice.java
@@ -0,0 +1,30 @@
+package ArraysInJava;
+
+/**
+ * Created by beatriceotugo on 17/07/2017.
+ */
+public class ComputerPrice {
+
+    public void displayStoreComputerPrice() {
+
+        //int computerPrice [] = new int[5];
+
+        int[] computerPrices = {50, 100, 900, 1500, 600};
+
+        for (int StorePrice : computerPrices) {
+            System.out.println(StorePrice);
+
+        }
+
+
+    }
+
+
+        public void displayOnlineComputerPrice() {
+            double [] price = {100.00, 200.00, 375.00, 1600.75, 655.99};
+            System.out.println(price[3]);
+
+
+
+    }
+}
