dc96b059a804a7634dba548f653fa04db336f168
Odili Ofoegbu <odcharles2k2@yahoo.com>
2017-07-18 15:51:37 +0100

    created new code using arraylist

---
+++ JavaPractice/src/Arraylist/MusicRunner.java
@@ -0,0 +1,13 @@
+package Arraylist;
+
+/**
+ * Created by oddie on 16/07/2017.
+ */
+public class MusicRunner {
+    public static void main(String[] args) {
+
+        Music music = new Music();
+        music.createMusicTypes();
+
+    }
+}
