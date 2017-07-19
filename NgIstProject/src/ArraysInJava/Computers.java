03f6b8c12aab57e653979770f5078a3dd9aa512f
beatrice-otugo <ngotugo@yahoo.co.uk>
2017-07-19 16:50:31 +0100

    updated code in Array

--- NgIstProject/src/ArraysInJava/Computers.java
+++ NgIstProject/src/ArraysInJava/Computers.java
@@ -1,12 +1,15 @@
 package ArraysInJava;
 
+import java.util.Arrays;
+import java.util.Scanner;
 /**
  * Created by beatriceotugo on 17/07/2017.
  */
 public class Computers {
 
+
     public void buyComputer() {
-        String [] ComputerBrands = new String[15];
+        String[] ComputerBrands = new String[10];
         ComputerBrands[0] = "hp";
         ComputerBrands[1] = "mac";
         ComputerBrands[2] = "lenovo";
@@ -18,22 +21,27 @@ public class Computers {
         ComputerBrands[8] = "fujitsu";
         ComputerBrands[9] = "compaq";
 
-        for(int x = 0; x < ComputerBrands.length; x++) {
+        for (int x = 0; x < ComputerBrands.length; x++) {
             System.out.println(ComputerBrands[x]);
 
-            //if (ComputerBrands[x] == "0") {
-                //System.out.println("These are all the computer brands I sell");
+            if (ComputerBrands[x].toLowerCase().contains("Hitachi".toLowerCase())) {
+                System.out.println("We do not sell Hitachi, please choose another computer brand");
+                break;
+            }
 
-            //}else{
-                //System.out.println("I do not sell such brand");
-            //}
-            //System.out.println(ComputerBrands[x]);
 
         }
 
-        //System.out.println(ComputerBrands.length);
+    }
 
-        //String[] hp = ComputerBrands.clone();
+
+    public void showAllStoreOutlets() {
+        String[] storeOutlet = {"london", "lancaster", "Aberystwyth", "Portcawl", "HemelHempstead", "Woking"};
+        for(String stores : storeOutlet) {
+            System.out.println(stores);
+
+            String[] outlet1 = storeOutlet.clone();
+        }
 
 
     }
