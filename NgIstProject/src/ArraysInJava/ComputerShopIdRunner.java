1ab86e6395da770f3e29fbb6c57d0eeb572793fd
beatrice-otugo <ngotugo@yahoo.co.uk>
2017-07-21 05:49:30 +0100

    added integer to 2D Array and modified existing code

---
+++ NgIstProject/src/ArraysInJava/ComputerShopIdRunner.java
@@ -0,0 +1,16 @@
+package ArraysInJava;
+
+/**
+ * Created by beatriceotugo on 20/07/2017.
+ */
+public class ComputerShopIdRunner {
+
+    public static void main(String[] args) {
+
+        ComputerShopId shopId = new ComputerShopId();
+
+        //shopId.getStoreId();
+
+        shopId.getShopno();
+    }
+}
