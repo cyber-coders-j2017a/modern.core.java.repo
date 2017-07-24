4028f0a139ebbf6c059fa61613fd3c021e819a16
Edwin Nwofor (Dotcom) <edwin.nwofor@yahoo.com>
2017-07-24 20:26:04 +0100

    Added a Church file to include method for going to church.

---
+++ ClassCodes/src/recapCodes/rev05Afonne/Church.java
@@ -0,0 +1,12 @@
+package recapCodes.rev05Afonne;
+
+/**
+ * Created by dotcom on 7/24/17.
+ */
+public class Church {
+
+
+    public void goToChurch(String name){
+        System.out.println(name + " is going to church");
+    }
+}
