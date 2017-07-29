716651ffa066ae0570f9faba2907041442de8faf
Tochukwu Anyanetoh <tccanini@yahoo.com>
2017-07-29 12:22:20 +0100

    corrected errors as commented on the review

--- Zoo/src/filesInJava/Geography.java
+++ Zoo/src/filesInJava/Geography.java
@@ -11,43 +11,41 @@ import java.io.IOException;
 public class Geography {
 
     public void writeToFile() throws IOException {
-        String filePath = "/home/admin-batch-a/Desktop/TestDevelopment/Projects/modern.core.java.repo/Zoo";
+        String filePath = "/home/admin-batch-a/Desktop/TestDevelopment/Projects/modern.core.java.repo/Zoo/designers.txt";
 
         FileWriter fileWriter = new FileWriter(filePath);
-        fileWriter.write("The list of geographical channels on multichoice \n");
+        fileWriter.write("The list of geographical channels on multichoice\n");
         fileWriter.write("Discovery Channel \n");
-        fileWriter.write("National Geographical Channel \n");
-        fileWriter.write("TAnimal Farm \n");
+        fileWriter.write("National Geographical Channel\n");
+        fileWriter.write("The Animal Farm\n");
 
         fileWriter.flush();
         fileWriter.close();
     }
 
     public void readDataFromFile() throws Exception {
-        String filePath = ("/home/admin-batch-a/Desktop/TestDevelopment/Projects/modern.core.java.repo/Zoo");
+        String filePath = "/home/admin-batch-a/Desktop/TestDevelopment/Projects/modern.core.java.repo/Zoo/designers.txt";
         FileReader fileReader = new FileReader(filePath);
-        char[] classArrays = new char[80];
+        char[]classArrays = new char[80];
         int result = fileReader.read(classArrays);
 
         for (char classArray : classArrays) {
             System.out.println(classArray);
 
-
         }
     }
 
-
     public void demoToCreateWriteAndReadFromFile() throws IOException {
-        File file = new File("designers.txt");
+        File file = new File("project.txt");
         file.createNewFile();
 
         FileWriter fileWriter = new FileWriter(file);
-        fileWriter.write ("Hugo Boss\n Gucci\n Tommy Hilfiger\n Armani\n Salvatore\n Dolce n Gabbana\n");
+        fileWriter.write ("Hugo Boss \n Gucci \n Tommy Hilfiger \n Armani \n Salvatore \n Dolce & Gabbana \n");
         fileWriter.flush();
         fileWriter.close();
 
         FileReader fileReader = new FileReader(file);
-        char[] charArrays = new char[50];
+        char[] charArrays = new char[100];
         fileReader.read(charArrays);
 
         for (char charArray : charArrays) {
