a3c0f2e9cd808f000befb8cbadc75c5fd28affdd
beatrice-otugo <ngotugo@yahoo.co.uk>
2017-08-11 07:10:44 +0100

    updated ArrayList code

--- NgIstProject/src/ArraysList/CourseTitleRunner.java
+++ NgIstProject/src/ArraysList/CourseTitleRunner.java
@@ -7,6 +7,15 @@ public class CourseTitleRunner {
 
     public static void main(String[] args) {
         CourseTitles courses = new CourseTitles();
+
         courses.displayAllCourses();
+
+        String addCourse = courses.addANewCourse("CorporateGovernance");
+        System.out.println(addCourse);
+
+        courses.retrieveACourse("Taxation");
+
+        boolean removeCourse = courses.deleteACourse();
+        System.out.println(removeCourse);
     }
 }
